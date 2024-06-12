0fb3053c9f13c5aa84a2f847ba891494d326ad08
JK <jenny.kaunistunturi@edunord.se>
2024-06-12 15:55:00 +0200

    Simple calculator that can do +, -, * and / equations.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+            Console.WriteLine("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter another number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Enter +, -, *, or /. ");
+            string item = Console.ReadLine();
+
+
+            if (item.Contains("+"))
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (item.Contains("-"))
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (item.Contains("*"))
+            {
+                Console.WriteLine(num1 * num2);
+            }
+
+            else
+            {
+                Console.WriteLine(num1 / num2);
+
+
+            }
+          }
         }
     }
-}
